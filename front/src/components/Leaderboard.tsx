d5a3e8f4c4d770920dc799655f5aa85e67b44b5e
Yeobu <stivefa@gmail.com>
2023-09-20 18:28:46 +0200

    Push leaderboard

--- front/src/components/Leaderboard.tsx
+++ front/src/components/Leaderboard.tsx
@@ -1,12 +1,186 @@
 import React from 'react';
 import './Leaderboard.css'; 
 
-const Leaderboard = () => {
-  return (
-    <div className="leaderboard">
-		LEADERBOARD TO DO
-    </div>
-  );
-}
+function Leaderboard() {
+    return (
+        <div className="Login">
+            <div className="VideoContainer">
+                <div className="Overlay"></div>
+                <div className="leaderboard_main_div">
+                    <div className="rank_div">
+                        <div className="position">
+                            <h1 className="h1">#1</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Pseudooooo</h1>
+                        </div>
+                        <div className="games_div">
+                            <h1 className="h1">#Games</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Wins</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Winrate%</h1>
+                        </div>
+                    </div>
+                    <div className="rank_div">
+                        <div className="position">
+                            <h1 className="h1">#1</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Pseudooooo</h1>
+                        </div>
+                        <div className="games_div">
+                            <h1 className="h1">#Games</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Wins</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Winrate%</h1>
+                        </div>
+                    </div>
+                    <div className="rank_div">
+                        <div className="position">
+                            <h1 className="h1">#1</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Pseudooooo</h1>
+                        </div>
+                        <div className="games_div">
+                            <h1 className="h1">#Games</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Wins</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Winrate%</h1>
+                        </div>
+                    </div>
+                    <div className="rank_div">
+                        <div className="position">
+                            <h1 className="h1">#1</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Pseudooooo</h1>
+                        </div>
+                        <div className="games_div">
+                            <h1 className="h1">#Games</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Wins</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Winrate%</h1>
+                        </div>
+                    </div>
+                    <div className="rank_div">
+                        <div className="position">
+                            <h1 className="h1">#1</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Pseudooooo</h1>
+                        </div>
+                        <div className="games_div">
+                            <h1 className="h1">#Games</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Wins</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Winrate%</h1>
+                        </div>
+                    </div>
+                    <div className="rank_div">
+                        <div className="position">
+                            <h1 className="h1">#1</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Pseudooooo</h1>
+                        </div>
+                        <div className="games_div">
+                            <h1 className="h1">#Games</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Wins</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Winrate%</h1>
+                        </div>
+                    </div>
+                    <div className="rank_div">
+                        <div className="position">
+                            <h1 className="h1">#1</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Pseudooooo</h1>
+                        </div>
+                        <div className="games_div">
+                            <h1 className="h1">#Games</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Wins</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Winrate%</h1>
+                        </div>
+                    </div>
+                    <div className="rank_div">
+                        <div className="position">
+                            <h1 className="h1">#1</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Pseudooooo</h1>
+                        </div>
+                        <div className="games_div">
+                            <h1 className="h1">#Games</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Wins</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Winrate%</h1>
+                        </div>
+                    </div>
+                    <div className="rank_div">
+                        <div className="position">
+                            <h1 className="h1">#1</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Pseudooooo</h1>
+                        </div>
+                        <div className="games_div">
+                            <h1 className="h1">#Games</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Wins</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Winrate%</h1>
+                        </div>
+                    </div>
+                    <div className="rank_div">
+                        <div className="position">
+                            <h1 className="h1">#1</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Pseudooooo</h1>
+                        </div>
+                        <div className="games_div">
+                            <h1 className="h1">#Games</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Wins</h1>
+                        </div>
+                        <div className="pseudo_div">
+                            <h1 className="h1">#Winrate%</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+  }
 
 export default Leaderboard;
