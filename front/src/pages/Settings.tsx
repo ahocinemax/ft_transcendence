74eef1fcc2f2f3f4363c517da8513a83304b2683
Yeobu <stive@DESKTOP-TCMBCOK>
2023-09-27 17:21:38 +0200

    Push settings

--- front/src/pages/Settings.tsx
+++ front/src/pages/Settings.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import './Settings.css'; 
+import React, { useState } from 'react';
+import './Settings.css';
 
 const Settings = () => {
+  const [is2FAEnabled, setIs2FAEnabled] = useState(false);
+
+  const toggle2FA = () => {
+    setIs2FAEnabled(!is2FAEnabled);
+  };
+
   return (
     <div className="settings">
-        <h1>Settings</h1>
+      <h1 className="Settingsh1">Settings</h1>
+      <div className="settings_container">
+        <div className="round_div_settings_img"></div>
+        <p className="info">#PlayerPseudo</p>
+        <p className="info">#Rank</p>
+        <div className="twofa_container">
+            <div className={`switch ${is2FAEnabled ? 'active' : ''}`} onClick={toggle2FA}>
+              <div className="switch-button"></div>
+            </div>
+            <span className="twoFA_status">
+              {is2FAEnabled ? '2FA Activé' : '2FA Désactivé'}
+            </span>
+        </div>
+      </div>
     </div>
   );
 }
