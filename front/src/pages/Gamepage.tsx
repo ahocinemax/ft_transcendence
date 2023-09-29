b9f41ba02d6bb5cc4fa68b936b8271267b577e63
FrizzLenovo <jahmimid@student.42.fr>
2023-09-30 01:47:03 +0200

    Petites modifs balle

--- front/src/pages/Gamepage.tsx
+++ front/src/pages/Gamepage.tsx
@@ -4,10 +4,10 @@ import './Gamepage.css';
 function Gamepage() {
 	const [playerBar1Y, setPlayerBar1Y] = useState(150);
 	const [playerBar2Y, setPlayerBar2Y] = useState(150);
-	// const [ballX, setBallX] = useState(400); // Position horizontale de la balle
-  	// const [ballY, setBallY] = useState(300); // Position verticale de la balle
-	// const [ballSpeedX, setBallSpeedX] = useState(5); // Vitesse horizontale de la balle
-	// const [ballSpeedY, setBallSpeedY] = useState(5); // Vitesse verticale de la balle
+	const [ballX, setBallX] = useState(0); // Position horizontale de la balle
+  	const [ballY, setBallY] = useState(0); // Position verticale de la balle
+	const [ballSpeedX, setBallSpeedX] = useState(50); // Vitesse horizontale de la balle
+	const [ballSpeedY, setBallSpeedY] = useState(50); // Vitesse verticale de la balle
 
 	const handleMouseMove1 = (event: any) => {
 		const mouseY = event.clientY; // Obtenez la position verticale de la souris
@@ -18,41 +18,42 @@ function Gamepage() {
 		const mouseY = event.clientY; // Obtenez la position verticale de la souris
 		setPlayerBar2Y(Math.min(Math.max(mouseY - 200, 0), 535)); // Ajustez la position de la deuxième barre du joueur
 	  };
-	
+	  
+	  let lastTimestamp = 0;
+	  
 	  useEffect(() => {
 		// Initialisation de la position de la balle au centre du terrain de jeu
-		// setBallX(400); // Position horizontale
-		// setBallY(300); // Position verticale
+		setBallX(250); // Position horizontale
+		setBallY(250); // Position verticale
 
-		// let lastTimestamp = 0;
 
 		// // Fonction pour mettre à jour la position de la balle
-		// const updateBallPosition = (timestamp) => {
-		// 	if (!lastTimestamp) {
-		// 		lastTimestamp = timestamp;
-		// 	}
-		// const deltaTime = timestamp - lastTimestamp;
-		// lastTimestamp = timestamp;
+		const updateBallPosition = (timestamp: number) => {
+			if (!lastTimestamp) {
+				lastTimestamp = timestamp;
+			}
+		const deltaTime = timestamp - lastTimestamp;
+		lastTimestamp = timestamp;
 
 		// // Calcul de la nouvelle position de la balle
-		// const newBallX = ballX + ballSpeedX * (deltaTime / 1000);
-		// const newBallY = ballY + ballSpeedY * (deltaTime / 1000);
+		const newBallX = ballX + ballSpeedX * (deltaTime / 1000);
+		const newBallY = ballY + ballSpeedY * (deltaTime / 1000);
 
 		// // Mettre à jour la position de la balle
-		// setBallX(newBallX);
-		// setBallY(newBallY);
+		setBallX(newBallX);
+		setBallY(newBallY);
 
-		// // Gérer les collisions avec les bords du terrain de jeu
-		// if (newBallY <= 0 || newBallY >= 600) {
-		// 	// Inverser la vitesse verticale en cas de collision avec le haut ou le bas
-		// 	setBallSpeedY(-ballSpeedY);
-		//   }
+		// Gérer les collisions avec les bords du terrain de jeu
+		if (newBallY <= 0 || newBallY >= 600) {
+		// Inverser la vitesse verticale en cas de collision avec le haut ou le bas
+			setBallSpeedY(-ballSpeedY);
+		  }
 	  
-		//   // Appeler la fonction de mise à jour à chaque frame du jeu
-		//   requestAnimationFrame(updateBallPosition);
-		// };
+		  // Appeler la fonction de mise à jour à chaque frame du jeu
+		  requestAnimationFrame(updateBallPosition);
+		};
 
-		// requestAnimationFrame(updateBallPosition);
+		requestAnimationFrame(updateBallPosition);
 
 		// Ajoutez un gestionnaire d'événements lors du montage du composant
 		window.addEventListener('mousemove', handleMouseMove1);
@@ -63,7 +64,7 @@ function Gamepage() {
 		  window.removeEventListener('mousemove', handleMouseMove1);
 		  window.removeEventListener('mousemove', handleMouseMove2);
 		};
-	  }, [/* ballX, ballY, ballSpeedX, ballSpeedY */]);
+	  }, [ballX, ballY, ballSpeedX, ballSpeedY]);
 
 
   return (
@@ -77,7 +78,7 @@ function Gamepage() {
 				<div className="Player2Area" style={{ right: 0 }}>
             		<div className="Playerbar2" style={{ top: playerBar2Y }}></div>
           		</div>
-				{/* <div className="Ball" style={{ left: ballX, top: ballY }}></div> */}
+				{<div className="Ball" style={{ left: ballX, top: ballY }}></div>}
 			</div>
 		</div>
 	</div>
